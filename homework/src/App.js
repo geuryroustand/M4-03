deef0bacf86c577d538628c6955f16ff780cd524
Geury Roustand <theromantic2010@hotmail.com>
2021-07-07 15:07:02 +0200

    MyBadge Done

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,11 +1,15 @@
 import WarningSign from "./Components/WarningSign";
+import MyBadge from "./Components/MyBadge";
+import { Container } from "react-bootstrap";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Container className="mt-5">
       <WarningSign text="Please be careful" />
-    </>
+      <MyBadge color="primary" text="Hi from a Badge" />
+    </Container>
   );
 }
 
